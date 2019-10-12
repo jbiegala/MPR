3d3162855a793941958f7bba6ed938848aba9cf4
jbiegala <jbiegala@gmail.com>
2019-10-12 09:43:19 +0200

    complete 1'st lesson

--- src/main/java/jakub/library/library/controller/BookController.java
+++ src/main/java/jakub/library/library/controller/BookController.java
@@ -1,10 +1,15 @@
 package jakub.library.library.controller;
 
+import jakub.library.library.model.Book;
 import jakub.library.library.service.BookService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+import java.util.List;
 
+@RestController
+@RequestMapping("/book")
 public class BookController {
     private final BookService bookService;
 
@@ -12,7 +17,8 @@ public class BookController {
         this.bookService = bookService;
     }
 
-    public BookService getBookService() {
-        return (BookService) bookService.findAll();
+    @GetMapping
+    public List<Book> getBookService() {
+        return bookService.findAll();
     }
 }
--- src/main/java/jakub/library/library/service/BookService.java
+++ src/main/java/jakub/library/library/service/BookService.java
@@ -2,12 +2,12 @@ package jakub.library.library.service;
 
 import jakub.library.library.model.Book;
 import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import java.util.ArrayList;
 import java.util.List;
 
 @Service
-
 public class BookService {
     public List<Book> findAll() {
         List<Book> books = new ArrayList<>();
