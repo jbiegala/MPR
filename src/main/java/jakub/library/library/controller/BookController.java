069555e8c905a171a06024bfe1d82a9e48d9e517
jbiegala <jbiegala@gmail.com>
2019-10-12 09:18:19 +0200

    add getBookService Controller's function

--- src/main/java/jakub/library/library/controller/BookController.java
+++ src/main/java/jakub/library/library/controller/BookController.java
@@ -1,4 +1,18 @@
 package jakub.library.library.controller;
 
+import jakub.library.library.service.BookService;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+
 public class BookController {
+    private final BookService bookService;
+
+    public BookController(BookService bookService) {
+        this.bookService = bookService;
+    }
+
+    public BookService getBookService() {
+        return (BookService) bookService.findAll();
+    }
 }
