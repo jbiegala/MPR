a96027835b13d83b157ce4d05da83a822aeb2056
jbiegala <jbiegala@gmail.com>
2019-10-12 09:06:31 +0200

    add findAll Service's function

--- src/main/java/jakub/library/library/service/BookService.java
+++ src/main/java/jakub/library/library/service/BookService.java
@@ -1,4 +1,32 @@
 package jakub.library.library.service;
 
+import jakub.library.library.model.Book;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+
 public class BookService {
+    public List<Book> findAll() {
+        List<Book> books = new ArrayList<>();
+
+        books.add(
+                new Book(
+                        1,
+                        "Ogniem i Mieczem",
+                        "Henryk Sienkiewicz"
+                )
+        );
+        books.add(
+                new Book(
+                        2,
+                        "Potop",
+                        "Henryk Sienkiewicz"
+                )
+        );
+
+        return books;
+    }
 }
